bf8ae3076abdd931723177354cfa2da612392432
salihbenli <salihbenli0@gmail.com>
2021-02-15 12:27:49 +0300

    10.Ders

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi.";
+        public static string ProductNameInvaild = "Ürün adı geçersiz.";
+        public static string ProductList = "Ürünler listelendi.";
+        public static string MaintenanceTime = "Ürünlerin listelenme zamanı geçti.";
+    }
+}
